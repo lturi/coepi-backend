1e31a6650754b73fb480d3f8efc75a381e58f430
Liz Turi <fiddledragon@gmail.com>
2020-03-09 00:17:34 -0400

    add framework for controllers to model

---
+++ app/controllers/case_bluetooth_device_controller.rb
@@ -0,0 +1,2 @@
+class CaseBluetoothDeviceController < ApplicationController
+end
---
+++ test/controllers/exposure_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ExposureControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
