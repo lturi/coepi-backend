edeccd9facd8abd7dd1afc0288884b127f188485
Liz Turi <fiddledragon@gmail.com>
2020-03-09 23:05:35 -0400

    start working on API - start with Client

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    resources :client, :client_bluetooth_device, :case, :case_bluetooth_device, :exposure, :subscription
-   
+
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :client, only: [:index]
+    end
+  end   
 end
